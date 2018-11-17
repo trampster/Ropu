9c2593c9ceae9409260d3f2d5a3c67a9b38656e4
Daniel Hughes <trampster@gmail.com>
2018-11-17 22:45:44 +1300

    Traded off some memory for extra performance in IntDictionary

--- Ropu.Shared/Concurrent/IntDictionary.cs
+++ Ropu.Shared/Concurrent/IntDictionary.cs
@@ -8,14 +8,14 @@ namespace Ropu.Shared.Concurrent
         const int _setSize = 256;
         int _count = 0;
 
-        readonly T[][][][] _store;
+        readonly T[][][] _store;
 
 
         public int Count => _count;
 
         public IntDictionary()
         {
-            _store = new T[_setSize][][][];
+            _store = new T[ushort.MaxValue][][];
         }
 
         class Cell
@@ -28,33 +28,25 @@ namespace Ropu.Shared.Concurrent
 
         public void Add(int index, T value)
         {
-            int index1 = index >> 24;
-            int index2 = (index >> 16) & 0xFF;
-            int index3 = (index >> 8) & 0xFF;
-            int index4 = index & 0xFF;
+            int index1 = index >> 16;
+            int index2 = (index >> 8) & 0xFF;
+            int index3 = index & 0xFF;
 
             var first = _store[index1];
             if(first == null)
             {
-                first = new T[_setSize][][];
+                first = new T[_setSize][];
                 _store[index1] = first;
             }
 
             var second = first[index2];
             if(second == null)
             {
-                second = new T[_setSize][];
+                second = new T[_setSize];
                 first[index2] = second;
             }
 
-            var third = second[index3];
-            if(third == null)
-            {
-                third = new T[_setSize];
-                second[index3] = third;
-            }
-
-            third[index4] = value;
+            second[index3] = value;
             _count++;
         }
 
@@ -62,21 +54,19 @@ namespace Ropu.Shared.Concurrent
         {
             get 
             { 
-                int index1 = index >> 24;
-                int index2 = (index >> 16) & 0xFF;
-                int index3 = (index >> 8) & 0xFF;
-                int index4 = index & 0xFF;
-                return _store[index1][index2][index3][index4];
+                int index1 = index >> 16;
+                int index2 = (index >> 8) & 0xFF;
+                int index3 = index & 0xFF;
+                return _store[index1][index2][index3];
             }
         }
 
         public void Remove(int index)
         {
-            int index1 = index >> 24;
-            int index2 = (index >> 16) & 0xFF;
-            int index3 = (index >> 8) & 0xFF;
-            int index4 = index & 0xFF;
-            _store[index1][index2][index3][index4] = default(T);
+            int index1 = index >> 16;
+            int index2 = (index >> 8) & 0xFF;
+            int index3 = index & 0xFF;
+            _store[index1][index2][index3] = default(T);
             _count--;
         }
 
