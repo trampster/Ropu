7aa0205e711bae8e32d4783725ff2318bfc3d647
Daniel Hughes <trampster@gmail.com>
2019-01-21 23:16:22 +1300

    Added ClientUI project using the cross platform Eto.Forms library.

---
+++ ClientUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace ClientUI
+{
+
+    public class MyForm : Form
+    {
+        public MyForm ()
+        {
+            Title = "My Cross-Platform App";
+            ClientSize = new Size(200, 200);
+            Content = new Label { Text = "Hello World!" };
+        }
+        
+        [STAThread]
+        static void Main()
+        {
+            new Application().Run(new MyForm());
+        }
+    }
+}
