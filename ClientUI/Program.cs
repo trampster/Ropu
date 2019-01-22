13ec2eecdd07d7123be96929e91fb5c571220416
hughesd <daniel.hughes@taitradio.com>
2019-01-22 14:01:59 +1300

    Added dsatabinding to ClientUI

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 using Eto.Forms;
 using Eto.Drawing;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace ClientUI
 {
+
+    public class MyModel : INotifyPropertyChanged
+    {
+        public MyModel()
+        {
+           var pttCommand = new Command();
+           pttCommand.Executed += (sender, args)  =>
+           {
+               State = "Clicked";
+           };
+           PttClickCommand = pttCommand;
+        }
+        string _state = "unregistered";
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                if (_state != value)
+                {
+                    _state = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        void OnPropertyChanged([CallerMemberName] string memberName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand PttClickCommand { get; }
+    }
 
     public class MyForm : Form
     {
         public MyForm ()
         {
-            Title = "My Cross-Platform App";
+            Title = "Ropu Client";
             ClientSize = new Size(200, 200);
-            Content = new Label { Text = "Hello World!" };
+
+            var stateLabel = new Label();
+            stateLabel.TextBinding.BindDataContext<MyModel>(m => m.State);
+            var button = new Button()
+            {
+                Text = "Push To Talk"
+            };
+            button.BindDataContext(c => c.Command, (MyModel model) => model.PttClickCommand);
+
+            Content = new TableLayout 
+                Rows = 
+                { 
+                    stateLabel, 
+                    button,
+                }
+            };
+            DataContext = new MyModel();
         }
         
         [STAThread]
