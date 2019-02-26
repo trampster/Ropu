a5868633460adb33241553ae1c9b59178f4d0a2d
Daniel Hughes <trampster@gmail.com>
2019-02-26 21:38:13 +1300

    Add a file audio source that plays wave files

---
+++ Client/FileAudio/FileAudioSource.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Ropu.Client.FileAudio
+{
+    public class FileAudioSource : IAudioSource
+    {
+        readonly FileStream _fileStream;
+        readonly Stopwatch _stopwatch = new Stopwatch();
+        readonly byte[] _buffer = new byte[320];
+        int _nextPlayTime;
+        public FileAudioSource(string path)
+        {
+            _fileStream = File.OpenRead(path);
+            SkipWavHeader();
+            _stopwatch.Start();
+        }
+
+        void SkipWavHeader()
+        {
+            _fileStream.Seek(44, SeekOrigin.Begin);
+        }
+
+        public void ReadAudio(short[] output)
+        {
+            if(_nextPlayTime < _stopwatch.ElapsedMilliseconds - 500)
+            {
+                _stopwatch.Reset();
+                _stopwatch.Start();
+                _nextPlayTime = 20;
+            }
+            int ammountRead = _fileStream.Read(_buffer, 0, 320);
+            if(ammountRead == 0)
+            {
+                //go back to start
+                SkipWavHeader();
+                _fileStream.Read(_buffer, 0, 320);
+            }
+            int outputIndex = 0;
+            for(int index = 0; index < _buffer.Length; index += 2)
+            {
+                var value = 
+                    (_buffer[index] ) + 
+                    (_buffer[index+1] << 8);
+                output[outputIndex] = (short)value;
+                outputIndex++;
+            }
+            
+            int waitTime = _nextPlayTime - (int)_stopwatch.ElapsedMilliseconds;
+            if(waitTime < 0)
+            {
+                Console.WriteLine($"Sleep time less than zero {waitTime}");
+                waitTime = 0;
+            }
+
+            System.Threading.Thread.Sleep(waitTime);
+
+            _nextPlayTime += 20;
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _fileStream.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
