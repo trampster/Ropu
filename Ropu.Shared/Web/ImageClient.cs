4fa73df986d096ac17422e81a0344cdba29a6063
Daniel Hughes <trampster@gmail.com>
2019-09-23 22:07:17 +1200

    Cache Images in client

--- Ropu.Shared/Web/ImageClient.cs
+++ Ropu.Shared/Web/ImageClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Ropu.Shared.Web
     public class ImageClient
     {
         readonly RopuWebClient _webClient;
+        readonly Dictionary<string, byte[]> _imageCache = new Dictionary<string, byte[]>();
         public ImageClient(RopuWebClient webClient)
         {
             _webClient = webClient;
@@ -14,10 +16,17 @@ namespace Ropu.Shared.Web
 
         public async Task<byte[]> GetImage(string hash)
         {
+            if(_imageCache.TryGetValue(hash, out byte[] imageBytes))
+            {
+                return imageBytes;
+            }
+            Console.WriteLine("GetImage");
             var response = await _webClient.Get<byte[]>($"api/Image/{hash}");
             if(response.StatusCode == HttpStatusCode.OK)
             {
-                return await response.GetBytes();
+                imageBytes = await response.GetBytes();
+                _imageCache[hash] = imageBytes;
+                return imageBytes;
             }
             Console.Error.WriteLine($"Failed to get image with hash {hash } Http Status Code {response.StatusCode}");
             return null;
